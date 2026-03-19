e88a53e5cd66b63f03ecb0230471261f01d078e7
maybanmarathon-sys <maybanmarathon@gmail.com>
2026-03-20 04:15:41 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const target = new Date("Oct 4, 2026 00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = target - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((distance / (1000 * 60)) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
